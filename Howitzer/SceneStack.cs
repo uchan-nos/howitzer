d02aa5766b374de106259cde41b041d6cbbd4647
uchan <uchan@github>
2012-11-20 17:58:50 +0900

    シーン遷移が正常に行えないバグを修正
    シーン遷移機能（Scene.CallSceneなど）をScene.Update()内部で呼び出すと、
    シーンスタックを走査中にシーンスタックが変更されてしまうバグを修正。

--- Howitzer/SceneStack.cs
+++ Howitzer/SceneStack.cs
@@ -8,6 +8,35 @@ namespace Howitzer
     class SceneStack
     {
         private List<Scene> scenes = new List<Scene>();
+        private List<Operation> nextOperations = new List<Operation>();
+
+        // scenesに対する1つの操作
+        private struct Operation
+        {
+            public enum Kind
+            {
+                Push, Pop
+            }
+
+            public Scene scene;
+            public Kind action;
+
+            public static Operation CreatePopOperation()
+            {
+                Operation o = new Operation();
+                o.scene = null;
+                o.action = Kind.Pop;
+                return o;
+            }
+
+            public static Operation CreatePushOperation(Scene s)
+            {
+                Operation o = new Operation();
+                o.scene = s;
+                o.action = Kind.Push;
+                return o;
+            }
+        }
 
         public Scene CurrentScene
         {
@@ -33,34 +62,55 @@ namespace Howitzer
 
         public void Push(Scene s)
         {
-            CurrentScene.Stop();
-            s.SetStack(this);
-            scenes.Add(s);
-            CurrentScene.Resume();
+            nextOperations.Add(Operation.CreatePushOperation(s));
         }
 
         public void Pop()
         {
-            if (scenes.Count >= 2)
-            {
-                CurrentScene.Stop();
-                CurrentScene.SetStack(null);
-                scenes.RemoveAt(scenes.Count - 1);
-                CurrentScene.Resume();
-            }
+            nextOperations.Add(Operation.CreatePopOperation());
         }
 
         public void ChangeCurrentScene(Scene newScene)
         {
-            CurrentScene.Stop();
-            CurrentScene.SetStack(null);
-            newScene.SetStack(this);
-            CurrentScene = newScene;
-            CurrentScene.Resume();
+            nextOperations.Add(Operation.CreatePopOperation());
+            nextOperations.Add(Operation.CreatePushOperation(newScene));
         }
 
         public void Update(GameStatus status)
         {
+            foreach (var op in nextOperations)
+            {
+                switch (op.action)
+                {
+                    case Operation.Kind.Pop:
+                        if (scenes.Count >= 2)
+                        {
+                            // 現在のシーンをまずポーズさせて、次にスタックとの接続を解除する
+                            CurrentScene.Pause();
+                            CurrentScene.SetStack(null);
+
+                            // スタックから削除する
+                            scenes.RemoveAt(scenes.Count - 1);
+
+                            // 新しく一番上になったシーンを再開させる
+                            CurrentScene.Resume();
+                        }
+                        break;
+                    case Operation.Kind.Push:
+                        // 現在のシーンをポーズさせる
+                        CurrentScene.Pause();
+
+                        // まずプッシュするシーンとスタックを連結させ、次にスタックに追加する
+                        op.scene.SetStack(this);
+                        scenes.Add(op.scene);
+
+                        // 新しく一番上になったシーンを開始させる
+                        CurrentScene.Resume();
+                        break;
+                }
+            }
+            nextOperations.Clear();
+
             foreach (var s in scenes)
             {
                 s.Update(status);
